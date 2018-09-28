daac04ccd0aa992a7f1f58c6b95000300115f3db
Rakesh Jha <29140@Hexaware.com>
2018-09-28 09:48:09 +0530

    webhook

--- GlassClaim/webhook.js
+++ GlassClaim/webhook.js
@@ -74,40 +74,55 @@ app.post('/claimCreate',function (req, res){
 //   var lossType=lossType;
 //   var lossCause=lossCause;
   // Data for creation 
-  var options = {
+ 
+
+ 
+
+  var options = { method: 'POST',
+  
     url: 'http://35.154.116.87:8080/cc/service/edge/fnol/cfnol',
-    headers:{
-      'Authorization': 'Basic c3U6Z3c=',
-      accept: 'application/json',
-      'content-type': 'application/json'
-    },
-    body:{
-      jsonrpc: '2.0',
- 
-      method: 'createClaimForHomeOwners',
-      params:[{
-        lossDate: '2018-09-27T00:00:00Z',
- 
-           lossType: 'PR',
- 
-           lossCause: 'glassbreakage',
- 
-           description: 'windowcrashed'
-
-      }]
-    },
-    json: true 
-    }
-    request(options, function (error, response, body) {
-
-      if (error) throw new Error(error);
-    
-     
-      console.log(response);
-      console.log(body);
-      console.log(error);
-    });
-      
+  
+    headers:
+  
+     { 'postman-token': 'ff149a5b-daaf-0000-0b8c-5301c162be75',
+  
+       'cache-control': 'no-cache',
+  
+       authorization: 'Basic c3U6Z3c=',
+  
+       accept: 'application/json',
+  
+       'content-type': 'application/json' },
+  
+    body:
+  
+     { jsonrpc: '2.0',
+  
+       method: 'createClaimForHomeOwners',
+  
+       params:
+  
+        [ { lossDate: '2018-09-27T00:00:00Z',
+  
+            lossType: 'PR',
+  
+            lossCause: 'glassbreakage',
+  
+            description: 'windowcrashed' } ] },
+  
+    json: true };
+  
+   
+  
+  request(options, function (error, response, body) {
+  
+    if (error) throw new Error(error);
+  
+   
+  
+    console.log(body);
+  
+  });
       
 })
 
