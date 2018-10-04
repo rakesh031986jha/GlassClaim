580dfae6c7cf28fa7f33687655ea1bfce704bdf4
Arun Prasaath <39402@Hexaware.com>
2018-10-04 09:56:44 +0530

    AP: Image border remove

--- GlassClaim/js/src/apiService.js
+++ GlassClaim/js/src/apiService.js
@@ -24,14 +24,46 @@ define(['jquery', 'settings', 'utils', 'messageTemplates', 'cards', 'uuid'],
             }
 
             userSays(userInput, callback) {
-
-                callback(null, messageTpl.userplaintext({
-                    "payload": userInput,
-                    "senderName": config.userTitle,
-                    "senderAvatar": config.userAvatar,
-                    "time": utils.currentTime(),
-                    "className": 'pull-right'
-                }));
+                if(userText == 'Auto'){
+                    callback(null, messageTpl.quickrepliesimg({
+                        "payload": userInput,
+                        "senderName": config.userTitle,
+                        "senderAvatar": config.userAvatar,
+                        "time": utils.currentTime(),
+                        "className": 'pull-right'
+                    }));
+                    let cardHTML = cards({
+                        "payload": [
+                          {
+                            "type":4,
+                            "platform":"facebook",
+                            "payload":{
+                              "facebook":{
+                                "text":"",
+                                "quick_replies_img":[{
+                                  "content_type":"text",
+                                  "title":"Auto",
+                                  "payload":"Auto"
+                                }]
+                              }
+                            }
+                          }
+                        ],
+                        "senderName": config.userTitle,
+                        "senderAvatar": config.userAvatar,
+                        "time": utils.currentTime(),
+                        "className": 'pull-right'
+                    }, "quickrepliesimg");
+                    callback(null, cardHTML);
+                } else {
+                    callback(null, messageTpl.userplaintext({
+                        "payload": userInput,
+                        "senderName": config.userTitle,
+                        "senderAvatar": config.userAvatar,
+                        "time": utils.currentTime(),
+                        "className": 'pull-right'
+                    }));
+                }
             }
             askBot(userInput, userText, callback) {
                 this.userSays(userText, callback);
