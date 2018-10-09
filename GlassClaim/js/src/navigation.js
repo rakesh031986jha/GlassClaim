7d0e5c0149536bee8eae65e994bf82814d68407a
rakesh031986jha <rakesh031986jha@gmail.com>
2018-10-09 12:49:55 +0530

    RK : Images removed

--- GlassClaim/js/src/navigation.js
+++
@@ -1,9 +0,0 @@
-define(['navigation'], function (navigation) {
-
-    var methods = {};
-    methods.data = {
-        "chatwindow": "Hi",
-        "roaming": "roaming"
-    }
-    return methods;
-});
